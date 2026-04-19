9a94fdeb55c6bb36de613f4dfd67d8a80f0c9318
muhammadhasnain3031 <muhammadhasnain3031@gmail.com>
2026-04-19 11:58:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import App from './App.js';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
