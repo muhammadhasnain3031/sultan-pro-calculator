d569382d5ac5be09478471b098ff46bb2d82b8fa
muhammadhasnain3031 <muhammadhasnain3031@gmail.com>
2026-04-19 13:17:51 -0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const rootElement = document.getElementById('root');
-const root = ReactDOM.createRoot(rootElement);
-
-root.render(
-  <React.StrictMode>
-    <window.App />
-  </React.StrictMode>
-);
