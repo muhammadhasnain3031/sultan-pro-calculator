6ca61b26e072ff54aafff32c4e3a1e869ed55259
muhammadhasnain3031 <muhammadhasnain3031@gmail.com>
2026-04-19 12:50:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-// Koi import nahi hona chahiye upar
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <window.App />
   </React.StrictMode>
 );
